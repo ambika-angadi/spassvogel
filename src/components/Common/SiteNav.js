447441467f8b21233861bada50ec734fb0a3cc45
Ambika <rudrambika.angadi@docc.techstarter.de>
2023-09-07 21:03:09 +0200

    final code

--- src/components/Common/SiteNav.js
+++ src/components/Common/SiteNav.js
@@ -1,3 +1,35 @@
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+
+
+// function SiteNav(props) {
+
+//     const handleLogout = () => {
+//         props.logOut();
+//     }
+
+//     return (
+//         <header>
+//            <Navbar bg="dark" expand="lg" variant="dark">
+
+//            <Container>
+//                     <Navbar.Brand><Nav.Link href="/">Spassvogel App</Nav.Link></Navbar.Brand>
+//                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                         <Navbar.Collapse id="basic-navbar-nav">
+//                             <Nav className="ms-md-auto">
+                           
+//                             <Nav.Link onClick={handleLogout}>Log out</Nav.Link>   
+//                             </Nav>
+//                         </Navbar.Collapse>
+//             </Container>
+//            </Navbar>
+
+//         </header>
+//     )
+// }
+
+// export default SiteNav;
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -11,19 +43,21 @@ function SiteNav(props) {
 
     return (
         <header>
-           <Navbar bg="dark" expand="lg" variant="dark">
+            <Navbar bg="white" expand="lg" variant="dark">
 
-           <Container>
-                    <Navbar.Brand><Nav.Link href="/">Spassvogel App</Nav.Link></Navbar.Brand>
+                <Container>
+                    <Navbar.Brand><Nav.Link href="/">
+                        <img  style={{borderRadius:'50%'}} src="/img/birdsiconok.png" />
+                    </Nav.Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ms-md-auto">
-                           
-                            <Nav.Link onClick={handleLogout}>Log out</Nav.Link>   
-                            </Nav>
-                        </Navbar.Collapse>
-            </Container>
-           </Navbar>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-md-auto">
+
+                            <Nav.Link onClick={handleLogout}>Ausloggen</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
         </header>
     )
--- src/components/static/StaticJokes.js
+++ src/components/static/StaticJokes.js
@@ -1,77 +1,177 @@
-import React  from 'react';
-import { PieChart, Pie,CartesianGrid, Tooltip, Legend, BarChart, Bar, XAxis, YAxis } from 'recharts';
+// import React  from 'react';
+// import { PieChart, Pie,CartesianGrid, Tooltip, Legend, BarChart, Bar, XAxis, YAxis } from 'recharts';
+
+
+// export default function StaticJokes() {
+
+//     const data = [
+//         {
+//           name: 'Page A',
+//           value:800000000
+//         },
+//         {
+//           name: 'Page B',
+//           value:1000000000 
+//         },
+//         {
+//           name: 'Page C',
+//           value:60000000000
+//         },
+//         {
+//           name: 'Page D',
+//           value: 30000000000
+//         },
+//         {
+//           name: 'Page E',
+//           value: 2000000000000
+//         },
+//         {
+//           name: 'Page F',
+//           value:900000000000
+//         },
+//         {
+//           name: 'Page G',
+//           value: 7000000000000
+//         },
+//       ];
+//   return (
+//     <div>
+        
+//           <div>
+//           <PieChart width={400} height={400}>
+//           <Pie
+//             dataKey="value"
+//             isAnimationActive={false}
+//             data={data}
+//             cx="200"
+//             cy="200"
+//             outerRadius={80}
+//             fill="#8884d8"
+//             label
+//           />
+//           <Tooltip />
+//           </PieChart>
+
+//           <BarChart
+//           width={500}
+//           height={300}
+//           data={data}
+//           margin={{
+//             top: 5,
+//             right: 30,
+//             left: 20,
+//             bottom: 5,
+//           }}
+//           barSize={20}
+//         >
+//           <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+//           <YAxis />
+//           <Tooltip />
+//           <Legend />
+//           <CartesianGrid strokeDasharray="3 3" />
+//           <Bar dataKey="value" fill="#8884d8" background={{ fill: '#eee' }} />
+//         </BarChart>
+//           </div>
+//     </div>
+//   )
+// };
+import React, { useState, useEffect } from 'react';
+
+import { listContacts } from '../../graphql/queries';
+import { API, graphqlOperation } from 'aws-amplify';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+
+import { CartesianGrid, Tooltip, Legend, BarChart, Bar, XAxis, YAxis } from 'recharts';
 
 
 export default function StaticJokes() {
+  const [contacts, setContacts] = useState([]);
 
-    const data = [
-        {
-          name: 'Page A',
-          value:800000000
-        },
-        {
-          name: 'Page B',
-          value:1000000000 
-        },
-        {
-          name: 'Page C',
-          value:60000000000
-        },
-        {
-          name: 'Page D',
-          value: 30000000000
-        },
-        {
-          name: 'Page E',
-          value: 2000000000000
-        },
-        {
-          name: 'Page F',
-          value:900000000000
-        },
-        {
-          name: 'Page G',
-          value: 7000000000000
-        },
-      ];
+
+
+
+  const getContacts = async () => {
+    try {
+      const contactsData = await API.graphql(graphqlOperation(listContacts, {
+        limit: 10,
+        sortField: "cell", // Sort by the "cell" field
+        sortDirection: "ASC", // Sort in ascending order
+
+      }));
+      console.log(contactsData);
+
+      const contactsList = contactsData.data.listContacts.items;
+      const sortedContacts = [...contactsList].sort((a, b) => a.cell.localeCompare(b.cell));
+      setContacts(sortedContacts);
+
+
+
+    } catch (err) {
+      console.log('error', err);
+    }
+  }
+
+  useEffect(() => {
+    getContacts()
+  }, []);
+
+
   return (
-    <div>
-        
-          <div>
-          <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={data}
-            cx="200"
-            cy="200"
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-          <Tooltip />
-          </PieChart>
 
-          <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          barSize={20}
-        >
-          <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="value" fill="#8884d8" background={{ fill: '#eee' }} />
-        </BarChart>
+
+    <Container className='contentPage'>
+      <Row className="px-4 my-5">
+        <Col><h1>Top Ten</h1></Col>
+      </Row>
+      <Row>
+        <Col sm={3}>
+          <div className="col-md-9 my-3 ">
+            <Link to={{ pathname: '/contacts' }} className='actionButton'>
+              Dashboard &gt;
+            </Link>
+
+            <Link to={{ pathname: '/quote' }} className='actionButton'>
+              Zitat  &gt;
+            </Link>
+            <Link to={{ pathname: '/dadjoke' }} className='actionButton'>
+              Jokes &gt;
+            </Link>
+           
           </div>
-    </div>
+        </Col>
+
+        <Col sm={9}>
+          <div className="col-md-9 px-4 my-2 mb-4" >
+            <BarChart
+              width={900}
+              height={300}
+              data={contacts}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              barSize={20}
+            >
+              <XAxis dataKey="" scale="point" padding={{ left: 10, right: 10 }} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <CartesianGrid strokeDasharray="7 7" />
+              <Bar dataKey="cell" fill="#8884d8" background={{ fill: '#eee' }} />
+            </BarChart>
+          </div>
+
+        </Col>
+
+      </Row>
+    </Container>
   )
 };
